bee4f06073cf0a43517e22d78cd6251bf74a86e7
rkmishracs <rkmishra165@gmail.com>
2018-09-18 18:15:43 +0400

    This is the first Program to Run the Java Program

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class HelloWorld
+{
+  public static void main (String[]args)
+  {
+
+    System.out.
+      println ("Hello World This is just test program to run the java code");
+  }
+
+}
